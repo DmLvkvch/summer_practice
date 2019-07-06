ea6a73ccf9cd37fb1875dfc236c2efec6e0aec2c
DmLvkvch <31544742+DmLvkvch@users.noreply.github.com>
2019-07-06 19:18:07 +0300

    Add files via upload

---
+++ topsort — копия/src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+import javax.swing.*;
+import java.awt.*;
+
+import static com.company.PAR_S.WINDOW_SIZE;
+
+public class Main extends JFrame {
+    public static void main(String[] args) {
+        Graph g = new Graph();
+        TopSort sort = new TopSort(g);
+        sort.Alg(g);
+        new Main(g, sort);
+    }
+    Main(Graph g, TopSort sort){
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        setTitle("TopSort");      //Имя окна
+        setSize( 1550, 800);                            //Размер окна
+
+        setResizable(false);
+
+        add( new Window(g, sort), BorderLayout.WEST );  // Размещение панельки с кнопками и изображением графа слева
+
+        setVisible(true);
+    }
+}
---
+++ topsort — копия/src/com/company/PAR_S.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import java.awt.*;
+
+public class PAR_S {
+    // Размеры полей
+    public final static Dimension WINDOW_SIZE = new Dimension(1400, 700);
+    public final static Dimension CREATE_GRAPH_PANEL_SIZE = new Dimension(300,200);
+    public final static Dimension SIZE_OF_INPUT_FIELD = new Dimension(25,19);
+    public final static Dimension SIZE_OF_GRAPH_FIELD = new Dimension(600,500);
+
+    // Цвета
+    public final static Color TEXT_COLOR = Color.BLACK;
+    public final static Color BUTTENS_BORDER = new Color(16, 77, 80);
+    public final static Color BACKGROUND = new Color(225, 219, 180);
+    public final static Color BASE_EDGE_COLOR = new Color(241, 237, 226);
+    public final static Color EDGE_LINE_COLOR = new Color(0, 0, 0);
+    public final static Color EDGE_CIRKLE_LINE_COLOR = new Color(0, 0, 0);
+    public final static Color CREATE_BUTTONS_BG = new Color(127, 186, 189);
+    public final static Color RESULT_EDGE_COLOR = new Color(203, 138, 82);
+    public final static Color BASE_VERTEX_COLOR = new Color(255, 255, 255);
+    public final static Color CIRCLE_BORDERLINE_COLOR = new Color(0, 0, 0);
+    public final static Color GRAPH_FIELD_BORDER = new Color(225, 219, 180);
+    public final static Color RESULT_VERTEX_COLOR = new Color(163, 98, 80);
+    public final static Color GRAPH_FIELD_BACKGROUND = new Color(50, 119, 123);
+
+    // Целочисленные константы
+    public final static int VERTEX_R = 15;
+    public final static int TEXT_SIZE = 14;
+    public final static int EDJE_CIRKLE_R = 28;
+    public final static int VERTEX_D = VERTEX_R*2;
+
+    // Размеры линий
+    public final static BasicStroke EDGE_LINE_THIKNESS = new BasicStroke(3);
+    public final static BasicStroke EDGE_CIRKLE_LINE_THKNESS = new BasicStroke(2);
+}
