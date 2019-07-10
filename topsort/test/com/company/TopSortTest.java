7866dc0cd2edfaa056c6d20ca52e6df6a52ac811
LosevMikhail <36499408+LosevMikhail@users.noreply.github.com>
2019-07-11 01:04:26 +0300

    реализовал один тест

--- topsort/test/com/company/TopSortTest.java
+++ topsort/test/com/company/TopSortTest.java
@@ -4,13 +4,20 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TopSortTest {
+    private static Graph graph;
 
     @BeforeEach
     void setUp() {
-        // TODO: implement
+        graph = new Graph(); // сбросили состояние графа, которое могло остаться после предыдущих тестов
+        // TODO: complete implementation
         /*
          Выполняется перед запуском каждого теста. Напрмер, здесь может быть задан граф,
          на котором будет выполняться тест, если он один и тот же для всех тестов.
@@ -24,7 +31,7 @@ class TopSortTest {
     }
 
     @Test
-    void alg() {
+    void algTest1() {
         // TODO: implement
         /*
         Тест метода alg.
@@ -38,6 +45,35 @@ class TopSortTest {
          */
         assertTrue( 1 == 1);
         assertEquals(1,1);
+    }
+    @Test
+    void algTest2() {
+        graph.addV(0);
+        graph.addV(1);
+        graph.addV(2);
+        graph.addE(0, 1);
+        graph.addE(0, 2);
+        graph.addE(1, 2);
+        TopSort topSort = new TopSort(graph);
+        topSort.alg();
+        List<Integer> actual = topSort.ans;
+
+        List<Integer> expected = new ArrayList<>();
+        expected.add(0);
+        expected.add(1);
+        expected.add(2);
+
+        // проверили ответ
+        assertEquals(expected, actual);
+
+
+        actual = topSort.getCycle();
+        expected = new LinkedList<Integer>();
+        // проверили поле цикл
+        assertEquals(expected, actual);
+
+
+        // остальные поля не проверяем, ибо они не могли быть изменены тестируемым методом
     }
 
     @Test
